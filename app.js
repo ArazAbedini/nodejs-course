417f7257695d209c212d27f285474162f8c1088a
ArazAbedini <araz.abedini1381@gmail.com>
2024-03-21 23:40:20 +0330

    remove json file

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 
-const messgae = require('./note.js')
+const notes = require('./note.js')
 const validator = require('validator')
 const yargs = require('yargs')
 const chalk = require('chalk')
+
 console.log(chalk.green.inverse.bold('green is the best color!'))
-const msg = messgae('hello')
-console.log(msg)
+// const msg = notes('hello')
+// console.log(msg)
 yargs.command({
     command: 'list',
     descript: 'list all command',
@@ -27,6 +28,22 @@ yargs.command({
     handler: function (argv) {
         console.log(argv.title)
     }
+})
+
+yargs.command({
+    command: 'remove',
+    descibe: 'remove existing json',
+    biulder: {
+        title: {
+            describe: 'note title',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function (argv) {
+        notes.remove(argv.title)
+    }
+    
 })
 yargs.parse()
 // const add = require('./utils.js')
--- note.js
+++ note.js
@@ -1,4 +1,44 @@
+const fs = require('fs')
 const message = function (notes){
     return 'Your notes is ' + notes + "!"
 }
-module.exports = message
+
+
+const addNotes = function (title, body){
+
+}
+
+const getNotes = function (title, body){
+
+}
+const removeNotes = function (title){
+    const notes = loadNotes()
+    const notesKeep =  notes.filter(function (note){
+        return note.title !== title
+    })
+
+    saveNotes(notesKeep)
+}
+
+const saveNotes = function (notes) {
+    const dataJSON = JSON.stringify(notes)
+    fs.writeFileSync('playground/1-json.json', dataJSON)
+}
+
+const loadNotes = function () {
+    try {
+        const dataBuffer = fs.readFileSync('playground/1-json.json')
+        const dataJSON = dataBuffer.toString()
+        return JSON.parse(dataJSON)
+    }catch(e) {
+        return []
+
+    }
+}
+
+
+module.exports = {
+    getNotes: getNotes,
+    addNotes: addNotes,
+    remove: removeNotes
+}
