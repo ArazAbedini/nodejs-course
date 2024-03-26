5e1417a2cfefb2a57f8766bf201049977d3ef07c
ArazAbedini <araz.abedini1381@gmail.com>
2024-03-26 12:14:39 +0330

    error handling

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,9 +1,15 @@
 const request = require('request')
 
-const url = 'http://api.weatherapi.com/v1/current.json?key=82992e45ce6749e3a24154608242503&q=Paris'
+const url = 'http://api.weatherapi.com/v1/current.json?key=82992e45ce6749e3a24154608242503&q=London'
 
 
-request({ url: url}, (error, response) => {
-    const data = JSON.parse(response.body)
-    console.log(data)
+request({ url: url, json: true}, (error, response) => {
+    // const data = JSON.parse(response.body)
+    if (error) {
+        console.log('Unable to connect weather service!')
+    } else if (response.body.error) {
+        console.log('Unable to find!')
+    } else { 
+        console.log(response.body.current)
+    }
 })
