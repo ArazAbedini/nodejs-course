c0c5bc9d6311555afca938b08831d3446a96fc10
ArazAbedini <araz.abedini1381@gmail.com>
2024-06-04 16:47:54 +0330

    complete web-servers

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,21 +1,38 @@
 const path = require('path')
 const express = require('express')
+const hbs = require('hbs')
 
 
 const app = express()
 const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
+console.log(partialsPath)
 
 app.use(express.static(publicDirectoryPath))
 app.set('view engine', 'hbs')
-
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
 
 
 
 
 app.get('', (req, res) => {
     res.render('index', {
-        title: 'Weather',
+        title: 'main',
         name: 'me!'
+    })
+})
+
+app.get('/about', (req,res) => {
+    res.render('about', {
+        title: 'about'
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        title: 'help'
     })
 })
 
@@ -30,6 +47,14 @@ app.get('/weather', (req, res) => {
 
 app.get('/about', (req, res) => {
     res.send()
+
+})
+
+
+app.get('*', (req, res) => {
+    res.render('404', {
+        page: 'desired page'
+    })
 })
 
 app.listen(3000, () => {
