db448def5986c58cd380013cf554f2489ffc5be5
ArazAbedini <araz.abedini1381@gmail.com>
2024-07-10 16:48:30 +0330

    add user models

--- task-manager/index.js
+++ task-manager/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 require('./src/db/mongoose')
 const Task = require('./src/models/tasks')
+const tasksRoutes = require('./src/routes/tasks')
+const userRoutes = require('./src/routes/user')
 
 
 
@@ -8,18 +10,11 @@ const app = express()
 const port = process.env.PORT || 3000
 
 
+
 app.use(express.json())
-
-app.post('/tasks', (req, res) => {
-     const task = Task(req.body)
-     task.save().then(() => {
-        res.send(task)
-     }).catch((e) => {
-        res.status(400).send(e)
-     })
-})
-
+app.use(tasksRoutes)
+app.use(userRoutes)
 
 app.listen(port, () => {
-    console.log('Server is up on port'+ port)
+    console.log('Server is up on port '+ port)
 })
